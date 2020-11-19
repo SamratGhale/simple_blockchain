56f8a79a17e481e1c01ac6d868bb83e9c32db672
SamratGhale <samrat.biz19@gmail.com>
2020-11-19 18:27:06 +0545

    need to add some css lmaooo

--- BlockChain.py
+++ BlockChain.py
@@ -1,54 +1,61 @@
 from hashlib import sha256
+import json
+
 class Block:
-    def __init__(self,prev,messege,nonce):
-        self.prev = prev
-        self.messege = messege
+    def __init__(self,messege,nonce,prev):
+        self.messege = messege 
         self.nonce = nonce
+        self.prev = prev
 
 class BlockChain:
     def __init__(self):
         self.chain = []
+        self.jsonChain = []
+        self.diff = 4
+
+    def genesisBlock(self):
+        self.chain.append(Block("This is this genesis block so it's hash and previous hash has no proof of work",0,"0"))
+        self.jsonChain.append(Block("This is this genesis block so it's hash and previous hash has no proof of work",0,"0").__dict__)
+
     def getHash(self,messege,nonce,prev):
-        return sha256(str(messege + str(nonce) +str(prev)).encode()).hexdigest()
-    def proofOfWork(self,messege):
+        return sha256(str(messege + str(nonce) + prev).encode()).hexdigest()
+
+    def printBlocks(self):
+        for i in self.chain:
+            print("prev hash:", i.prev)
+            print("messege:" , i.messege)
+            print("Nonce:" , i.nonce)
+            print("\n\n")
+
+    def proofOfWork(self,messege, prev):
         nonce = 0
-        #prev = self.chain[-1].prev
-        prev = self.getPrevHash()
-        h = self.getHash(messege,nonce,prev)
-        while(h[:4] !="0000"):
-            h = self.getHash(messege,nonce,prev)
+        while(self.getHash(messege,nonce,prev)[:self.diff] != "0"*self.diff):
             nonce +=1
         return nonce
-    def getPrevHash(self):
-        m = self.chain[-1].messege
-        n = self.chain[-1].nonce
-        p = self.chain[-1].prev
-        return self.getHash(m,n,p)
+
     def addBlock(self,messege):
-        nonce = self.proofOfWork(messege)
-        self.chain.append(Block(self.getPrevHash(),messege,nonce))
-    def genesisBlock(self):
-        self.chain.append(Block(0,"0",0))
-    def printChain(self):
-        for i in self.chain:
-            print("messege:",i.messege)
-            print("previous hash:",i.prev)
-            print("nonce:",i.nonce)
+        pBlock = self.chain[-1]
+        h = self.getHash(pBlock.messege,pBlock.nonce,pBlock.prev)
+        nonce = self.proofOfWork(messege,h)
+        print("block added:",h)
+        self.chain.append(Block(messege,nonce,h))
+        self.jsonChain.append(Block(messege,nonce,h).__dict__)
 
     def validate(self):
-        """
+        valid = []
         for i in self.chain:
             h = self.getHash(i.messege,i.nonce,i.prev)
-            valid.append(h[:4] == "0000")
-            print(h)
+            valid.append(h[:self.diff] == "0"*self.diff)
         valid = valid[1:]
         return all(valid)
-        """
-        print(self.getPrevHash())
 
 if __name__ == "__main__":
     a = BlockChain()
     a.genesisBlock()
-    a.addBlock("samrat")
-    a.printChain()
-    a.validate()
+    a.addBlock("this is the first block")
+    a.addBlock("this is the second block")
+    a.addBlock("this is the third block")
+    #a.chain[1].messege= "apple"
+    a.printBlocks()
+    print(a.jsonChain[0])
+    print(a.validate())
--- server.py
+++ server.py
@@ -1,2 +1,23 @@
-from flask import Flask , jsonify
-from 
+from flask import Flask , jsonify, render_template, request
+from BlockChain import  BlockChain
+import json
+
+app = Flask(__name__)
+
+blockS = BlockChain()
+blockS.genesisBlock()
+blockS.addBlock("samrat ghale")
+
+@app.route('/',methods=['GET','POST'])
+def index():
+    return render_template("index.html")
+
+@app.route('/get_chain',methods=['GET','POST'])
+def get_chain():
+    if request.method == 'POST':
+        messege = request.form.get('messege')
+        blockS.addBlock(messege)
+    return jsonify(blockS.jsonChain)
+
+if __name__=="__main__":
+    app.run(host="127.0.0.1",port=5000,debug=True)
